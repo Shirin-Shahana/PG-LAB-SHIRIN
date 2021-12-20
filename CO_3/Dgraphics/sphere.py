e2dcb06b1c61169c567eb6487d6cae86005cc5ad
Shirin-Shahana <93868520+Shirin-Shahana@users.noreply.github.com>
2021-12-20 22:29:57 +0530

    Add files via upload

---
+++ CO_3/Dgraphics/sphere.py
@@ -0,0 +1,7 @@
+pi=3.14
+def sphere_volume(pi,r):
+    return (4/3)*pi*r*r*r
+def circumference(pi,r):
+    return 2*pi*r
+def surf_area(pi,r):
+    return 4*pi*r*r
--- CO_3/graphicsuse.py
+++ CO_3/graphicsuse.py
@@ -1,6 +1,8 @@
 from graphics import rectangle
 from graphics import circle
 
+from Dgraphics import *  #import cuboid and sphere
+
 print("Read values: \n Rectangle:\n")
 l=int(input("Enter length"))
 b=int(input("Enter breadth"))
@@ -13,7 +15,31 @@ r=int(input("Enter radius"))
 area=circle.area(circle.pi,r)
 perimeter=circle.perimeter(circle.pi,r)
 
+print("Read values: \n Cuboid:\n")
+l=int(input("Enter length"))
+b=int(input("Enter breadth"))
+h=int(input("Enter height"))
+c_area=cuboid.area(l,b,h)
+c_perimeter=cuboid.perimeter(l,b,h)
+
+print("Read values: \n Sphere:\n")
+r=int(input("Enter radius"))
+sphere_surf_area=sphere.surf_area(sphere.pi,r)
+sphere_circumference=sphere.circumference(sphere.pi,r)
+sphere_volume=sphere.sphere_volume(sphere.pi,r)
+
 print("Area of Rectangle:",r_area)
-print("Area of Circle:",area)
 print("Perimeter of Rectangle:",r_perimeter)
+
+print("Area of Circle:",area)
 print("Perimeter of Circle:",perimeter)
+
+
+
+print("Area of cuboid:",c_area)
+print("Perimeter of cuboid:",c_perimeter)
+
+print("surface Area of sphere:",sphere_surf_area)
+print("circumference of sphere:",sphere_circumference)
+print("sphere_volume:",sphere_volume)
+
